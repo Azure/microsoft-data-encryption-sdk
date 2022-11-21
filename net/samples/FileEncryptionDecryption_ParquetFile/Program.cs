6d779334be29c5b52714f08f36a31ab74b3871fc
Cheena Malhotra <cmalhotra@microsoft.com>
2022-11-21 12:31:13 -0800

    Add decryption sample

--- net/samples/FileEncryptionDecryption_ParquetFile/Program.cs
+++ net/samples/FileEncryptionDecryption_ParquetFile/Program.cs
@@ -9,8 +9,10 @@ using Microsoft.Data.Encryption.Cryptography;
 using Microsoft.Data.Encryption.Cryptography.Serializers;
 using Microsoft.Data.Encryption.FileEncryption;
 
-namespace FileEncryptionDecryption_ParquetFile {
-    public class Program {
+namespace FileEncryptionDecryption_ParquetFile
+{
+    public class Program
+    {
         /// <summary>
         /// TODO: Provide AKV Key URL here to run this sample.
         /// </summary>
@@ -20,46 +22,91 @@ namespace FileEncryptionDecryption_ParquetFile {
         public static readonly TokenCredential TokenCredential = new InteractiveBrowserCredential();
 
         // Azure Key Vault provider that allows client applications to access a key encryption key is stored in Microsoft Azure Key Vault.
-        public static readonly EncryptionKeyStoreProvider azureKeyProvider = new AzureKeyVaultKeyStoreProvider (TokenCredential);
+        public static readonly EncryptionKeyStoreProvider azureKeyProvider = new AzureKeyVaultKeyStoreProvider(TokenCredential);
 
         // Represents the key encryption key that encrypts and decrypts the data encryption key
-        public static readonly KeyEncryptionKey keyEncryptionKey = new KeyEncryptionKey ("KEK", AzureKeyVaultKeyPath, azureKeyProvider);
+        public static readonly KeyEncryptionKey keyEncryptionKey = new("KEK", AzureKeyVaultKeyPath, azureKeyProvider);
 
         // Represents the encryption key that encrypts and decrypts the data items
-        public static readonly ProtectedDataEncryptionKey encryptionKey = new ProtectedDataEncryptionKey ("DEK", keyEncryptionKey);
+        public static readonly ProtectedDataEncryptionKey encryptionKey = new("DEK", keyEncryptionKey);
 
-        public static void Main () {
+        public static void Main()
+        {
             // open input and output file streams
-            Stream inputFile = File.OpenRead (".\\resources\\userdata1.parquet");
-            Stream outputFile = File.OpenWrite (".\\resources\\out1.parquet");
+            using Stream inputFileForEncryption = File.OpenRead(".\\resources\\userdata1.parquet");
+            using Stream encryptedFile = File.OpenWrite(".\\resources\\encryptedUserData.parquet");
+            PerformEncryption(inputFile: inputFileForEncryption, outputFile: encryptedFile);
+            encryptedFile.Close();
 
+            using Stream inputFileForDecryption = File.OpenRead(".\\resources\\encryptedUserData.parquet");
+            using Stream decryptedFile = File.OpenWrite(".\\resources\\decryptedUserdata.parquet");
+            PerformDecryption(inputFile: inputFileForDecryption, outputFile: decryptedFile);
+        }
+
+        private static void PerformEncryption(Stream inputFile, Stream outputFile)
+        {
             // Create reader
-            using ParquetFileReader reader = new ParquetFileReader (inputFile);
+            using ParquetFileReader reader = new(inputFile);
 
             // Copy source settings as target settings
             List<FileEncryptionSettings> writerSettings = reader.FileEncryptionSettings
-                .Select (s => Copy (s))
-                .ToList ();
+                .Select(s => Copy(s))
+                .ToList();
 
             // Modify a few column settings
-            writerSettings[0] = new FileEncryptionSettings<DateTimeOffset?> (encryptionKey, SqlSerializerFactory.Default.GetDefaultSerializer<DateTimeOffset?> ());
-            writerSettings[3] = new FileEncryptionSettings<string> (encryptionKey, EncryptionType.Deterministic, new SqlVarCharSerializer (size: 255));
-            writerSettings[10] = new FileEncryptionSettings<double?> (encryptionKey, StandardSerializerFactory.Default.GetDefaultSerializer<double?> ());
+            writerSettings[0] = new FileEncryptionSettings<DateTimeOffset?>(encryptionKey, SqlSerializerFactory.Default.GetDefaultSerializer<DateTimeOffset?>());
+            writerSettings[3] = new FileEncryptionSettings<string>(encryptionKey, EncryptionType.Deterministic, new SqlVarCharSerializer(size: 255));
+            writerSettings[10] = new FileEncryptionSettings<double?>(encryptionKey, StandardSerializerFactory.Default.GetDefaultSerializer<double?>());
 
             // Create and pass the target settings to the writer
-            using ParquetFileWriter writer = new ParquetFileWriter (outputFile, writerSettings);
+            using ParquetFileWriter writer = new(outputFile, writerSettings);
 
             // Process the file
-            ColumnarCryptographer cryptographer = new ColumnarCryptographer (reader, writer);
-            cryptographer.Transform ();
+            ColumnarCryptographer cryptographer = new(reader, writer);
+            cryptographer.Transform();
 
-            Console.WriteLine ($"Parquet File processed successfully. Verify output file contains encrypted data.");
+            Console.WriteLine($"Parquet File encrypted successfully. Verify output file contains encrypted data.");
         }
 
-        public static FileEncryptionSettings Copy (FileEncryptionSettings encryptionSettings) {
-            Type genericType = encryptionSettings.GetType ().GenericTypeArguments[0];
-            Type settingsType = typeof (FileEncryptionSettings<>).MakeGenericType (genericType);
-            return (FileEncryptionSettings) Activator.CreateInstance (
+        private static void PerformDecryption(Stream inputFile, Stream outputFile)
+        {
+            // Create reader
+            using ParquetFileReader reader = new(file: inputFile,
+                encryptionKeyStoreProviders: new Dictionary<string, EncryptionKeyStoreProvider> 
+                { 
+                    [azureKeyProvider.ProviderName] = azureKeyProvider 
+                }
+            );
+
+            // Copy source settings as target settings
+            var writerSettings = reader.FileEncryptionSettings
+                .Select(s => Copy(s))
+                .ToList();
+
+            var targetColumnTypes = reader.FileEncryptionSettings
+                .Select(s => s.GetSerializer().GetType())
+                .ToList();
+
+            // Modify a few column settings
+            writerSettings[0] = new FileEncryptionSettings<DateTimeOffset?>(encryptionKey, EncryptionType.Plaintext, SqlSerializerFactory.Default.GetDefaultSerializer<DateTimeOffset?>());
+            writerSettings[3] = new FileEncryptionSettings<string>(encryptionKey, EncryptionType.Plaintext, SqlSerializerFactory.Default.GetDefaultSerializer<string?>());
+            writerSettings[10] = new FileEncryptionSettings<double?>(encryptionKey, EncryptionType.Plaintext, StandardSerializerFactory.Default.GetDefaultSerializer<double?>());
+
+            // Create and pass the target settings to the writer
+            using ParquetFileWriter writer = new(outputFile, writerSettings);
+
+            // Process the file
+            ColumnarCryptographer cryptographer = new(reader, writer);
+            cryptographer.Transform();
+
+            Console.WriteLine($"Parquet File decrypted successfully. Verify output file contains the original file data.");
+        }
+
+        public static FileEncryptionSettings Copy(FileEncryptionSettings encryptionSettings)
+        {
+            Type genericType = encryptionSettings.GetType().GenericTypeArguments[0];
+            Type settingsType = typeof(FileEncryptionSettings<>).MakeGenericType(genericType);
+            return (FileEncryptionSettings)Activator.CreateInstance(
                 settingsType,
                 new object[] {
                     encryptionSettings.DataEncryptionKey,
